b17c4b27094a6b767ec844e530cbaf6535cdad1d
Przemek Grondek <przemek@grondek.pl>
2023-02-05 16:34:01 +0100

    Add duplicity

---
+++ roles/duplicity/files/backup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+source  /home/duplicity/email-settings.sh
+
+MAX_TIME="3M"
+FULL_BACKUP_EVERY="1M"
+SOURCE="/mnt/MAIN"
+DESTINATION="onedrive://duplicity/nas"
+
+LOG="/home/duplicity/backup.log"
+
+rm $LOG
+duplicity remove-older-than $MAX_TIME $DESTINATION >> $LOG
+duplicity --full-if-older-than $FULL_BACKUP_EVERY $SOURCE $DESTINATION >> $LOG
+
+sendemail \
+  -f "$EMAIL_SENDER" \
+  -t "$EMAIL_RECEIPIENT" \
+  -u "Duplicity backup log" \
+  -message-file=$LOG \
+  -s "$EMAIL_SERVER" \
+  -xu "$EMAIL_USER" \
+  -xp "$EMAIL_PASSWORD" \
+  -o "tls=$EMAIL_TLS"
